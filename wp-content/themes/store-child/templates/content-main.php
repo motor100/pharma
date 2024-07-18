784ce1f8fa188edb40c595aac2b68edc7a62ac1b
Evgeny <260443@mail.ru>
2024-07-18 15:51:17 +0500

    Home page remove .row tag

--- wp-content/themes/store-child/templates/content-main.php
+++ wp-content/themes/store-child/templates/content-main.php
@@ -43,14 +43,36 @@ jQuery(document).ready(function($) {
 	<div class="catalog-tabs">
 		<div class="groups">
 			<div class="container">
-				<div class="row">				
-					<ul class="clear">
-						<li><a href="#tabs-1"><div class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/includes/images/catalog-groups/1.png" alt=""></div>по сериям</a></li>
-						<li><a href="#tabs-2"><div class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/includes/images/catalog-groups/2.png" alt=""></div>по направлениям</a></li>
-						<li><a href="#tabs-3"><div class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/includes/images/catalog-groups/3.png" alt=""></div>подбор</a></li>
-						<li><a href="#tabs-4"><div class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/includes/images/catalog-groups/4.png" alt=""></div>обучение</a></li>
-					</ul>
-				</div>
+				<ul class="clear">
+					<li class="catalog-tabs-item">
+						<a href="#tabs-1">
+							<div class="img">
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/includes/images/catalog-groups/1.png" alt="">
+							</div>по сериям
+						</a>
+					</li>
+					<li class="catalog-tabs-item">
+						<a href="#tabs-2">
+							<div class="img">
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/includes/images/catalog-groups/2.png" alt="">
+							</div>по направлениям
+						</a>
+					</li>
+					<li class="catalog-tabs-item">
+						<a href="#tabs-3">
+							<div class="img">
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/includes/images/catalog-groups/3.png" alt="">
+							</div>подбор
+						</a>
+					</li>
+					<li class="catalog-tabs-item">
+						<a href="#tabs-4">
+							<div class="img">
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/includes/images/catalog-groups/4.png" alt="">
+							</div>обучение
+						</a>
+					</li>
+				</ul>
 			</div>
 		</div>
 		<div class="curved green v2">
@@ -59,78 +81,76 @@ jQuery(document).ready(function($) {
 			<div class="three"></div>
 		</div>
 		<div class="container">
-			<div class="row">
-				<div id="tabs-1">
-					<div class="cat-wrapper">
-						<?php render__catalog('183'); ?>
-					</div>
+			<div id="tabs-1">
+				<div class="cat-wrapper">
+					<?php render__catalog('183'); ?>
 				</div>
-				<div id="tabs-2">
-					<div class="cat-wrapper">
-						<?php render__catalog('96'); ?>
-					</div>
+			</div>
+			<div id="tabs-2">
+				<div class="cat-wrapper">
+					<?php render__catalog('96'); ?>
 				</div>
-				<div id="tabs-3">
-					<div class="cat-wrapper">
-						<?php
-						$args = array(
-						'tax_query' => array(
-						'relation' => 'AND',
-						array(
-						'taxonomy' => 'product_cat',
-						'field' => 'slug',
-						'terms' => 'klasternii_analiz'
-						)),
-						'posts_per_page' => 20, 
-						'post_type' => 'product', 
-						'orderby' => 'title',
-						);
-						$loop = new WP_Query( $args );
-						while ( $loop->have_posts() ) : $loop->the_post();
-						global $product;
-						?>
-						<div class="children__item">
-							<div class="children__image">
-								<a href="<?php echo get_permalink( $loop->post->ID ) ?>">
-									<img class="ww100" src="<?php echo get_the_post_thumbnail_url($loop->post->ID); ?>" alt="">
-								</a>
-							</div>
-							<a class="subcat__link" href="<?php echo get_permalink( $loop->post->ID ) ?>"><?php the_title(); ?></a>
+			</div>
+			<div id="tabs-3">
+				<div class="cat-wrapper">
+					<?php
+					$args = array(
+					'tax_query' => array(
+					'relation' => 'AND',
+					array(
+					'taxonomy' => 'product_cat',
+					'field' => 'slug',
+					'terms' => 'klasternii_analiz'
+					)),
+					'posts_per_page' => 20, 
+					'post_type' => 'product', 
+					'orderby' => 'title',
+					);
+					$loop = new WP_Query( $args );
+					while ( $loop->have_posts() ) : $loop->the_post();
+					global $product;
+					?>
+					<div class="children__item">
+						<div class="children__image">
+							<a href="<?php echo get_permalink( $loop->post->ID ) ?>">
+								<img class="ww100" src="<?php echo get_the_post_thumbnail_url($loop->post->ID); ?>" alt="">
+							</a>
 						</div>
-						<?php endwhile; ?>
-						<?php wp_reset_query(); ?>
+						<a class="subcat__link" href="<?php echo get_permalink( $loop->post->ID ) ?>"><?php the_title(); ?></a>
 					</div>
+					<?php endwhile; ?>
+					<?php wp_reset_query(); ?>
 				</div>
-				<div id="tabs-4">
-					<div class="cat-wrapper">
-						<?php
-						$args = array(
-						'tax_query' => array(
-						'relation' => 'AND',
-						array(
-						'taxonomy' => 'product_cat',
-						'field' => 'slug',
-						'terms' => 'vebinary'
-						)),
-						'posts_per_page' => 20, 
-						'post_type' => 'product', 
-						'orderby' => 'title',
-						);
-						$loop = new WP_Query( $args );
-						while ( $loop->have_posts() ) : $loop->the_post();
-						global $product;
-						?>
-						<div class="children__item">
-							<div class="children__image">
-								<a href="<?php echo get_permalink( $loop->post->ID ) ?>">
-									<img class="ww100" src="<?php echo get_the_post_thumbnail_url($loop->post->ID); ?>" alt="">
-								</a>
-							</div>
-							<a class="subcat__link" href="<?php echo get_permalink( $loop->post->ID ) ?>"><?php the_title(); ?></a>
+			</div>
+			<div id="tabs-4">
+				<div class="cat-wrapper">
+					<?php
+					$args = array(
+					'tax_query' => array(
+					'relation' => 'AND',
+					array(
+					'taxonomy' => 'product_cat',
+					'field' => 'slug',
+					'terms' => 'vebinary'
+					)),
+					'posts_per_page' => 20, 
+					'post_type' => 'product', 
+					'orderby' => 'title',
+					);
+					$loop = new WP_Query( $args );
+					while ( $loop->have_posts() ) : $loop->the_post();
+					global $product;
+					?>
+					<div class="children__item">
+						<div class="children__image">
+							<a href="<?php echo get_permalink( $loop->post->ID ) ?>">
+								<img class="ww100" src="<?php echo get_the_post_thumbnail_url($loop->post->ID); ?>" alt="">
+							</a>
 						</div>
-						<?php endwhile; ?>
-						<?php wp_reset_query(); ?>
+						<a class="subcat__link" href="<?php echo get_permalink( $loop->post->ID ) ?>"><?php the_title(); ?></a>
 					</div>
+					<?php endwhile; ?>
+					<?php wp_reset_query(); ?>
 				</div>
 			</div>
 		</div>
@@ -140,128 +160,74 @@ jQuery(document).ready(function($) {
 <div class="offer m-b-200">
 	<div class="title">Предложение недели</div>
 	<div class="container">
-        <div class="row">
-			<div class="all-offer">
-				<div class="items">
-					<?php
-						$args = array(
-						'tax_query' => array(
-						'relation' => 'AND',
-						array(
-						'taxonomy' => 'product_cat',
-						'field' => 'slug',
-						'terms' => 'predlozhenie-4'
-						)),
-						'posts_per_page' => 4, 
-						'post_type' => 'product', 
-						'orderby' => 'title',
-						);
-						$loop = new WP_Query( $args );
-						while ( $loop->have_posts() ) : $loop->the_post();
-						global $product;
-						if (get_post_meta( get_the_ID(), '_sale_price', true)) {$price = get_post_meta( get_the_ID(), '_sale_price', true); } else
-							{$price = get_post_meta( get_the_ID(), '_regular_price', true);}
-					?>
-					<div class="children__item">
-						<div class="children__image">
-							<div class="circle-green favorite"></div>
-							<a href="<?php echo get_permalink( $loop->post->ID ) ?>">
-								<img src="<?php echo get_the_post_thumbnail_url($loop->post->ID); ?>" alt="<?php the_title(); ?>">
-							</a>
-						</div>
-						<a class="subcat__link m-b-10" href="<?php echo get_permalink( $loop->post->ID ) ?>"><?php the_title(); ?></a>
-						<div class="price black"><span><? echo $price; ?> &#8381;</span></div>
-						
+		<div class="all-offer">
+			<div class="items">
+				<?php
+					$args = array(
+					'tax_query' => array(
+					'relation' => 'AND',
+					array(
+					'taxonomy' => 'product_cat',
+					'field' => 'slug',
+					'terms' => 'predlozhenie-4'
+					)),
+					'posts_per_page' => 4, 
+					'post_type' => 'product', 
+					'orderby' => 'title',
+					);
+					$loop = new WP_Query( $args );
+					while ( $loop->have_posts() ) : $loop->the_post();
+					global $product;
+					if (get_post_meta( get_the_ID(), '_sale_price', true)) {$price = get_post_meta( get_the_ID(), '_sale_price', true); } else
+						{$price = get_post_meta( get_the_ID(), '_regular_price', true);}
+				?>
+				<div class="children__item">
+					<div class="children__image">
+						<div class="circle-green favorite"></div>
+						<a href="<?php echo get_permalink( $loop->post->ID ) ?>">
+							<img src="<?php echo get_the_post_thumbnail_url($loop->post->ID); ?>" alt="<?php the_title(); ?>">
+						</a>
 					</div>
-					<?php endwhile; ?>
-					<?php wp_reset_query(); ?>
+					<a class="subcat__link m-b-10" href="<?php echo get_permalink( $loop->post->ID ) ?>"><?php the_title(); ?></a>
+					<div class="price black"><span><? echo $price; ?> &#8381;</span></div>
+					
 				</div>
-				<div class="item">
-					<?php
-						$args = array(
-						'tax_query' => array(
-						'relation' => 'AND',
-						array(
-						'taxonomy' => 'product_cat',
-						'field' => 'slug',
-						'terms' => 'promo'
-						)),
-						'posts_per_page' => 1, 
-						'post_type' => 'product', 
-						'orderby' => 'title',
-						);
-						$loop = new WP_Query( $args );
-						while ( $loop->have_posts() ) : $loop->the_post();
-						global $product;
-						if (get_post_meta( get_the_ID(), '_sale_price', true)) {$price = get_post_meta( get_the_ID(), '_sale_price', true); } else
-							{$price = get_post_meta( get_the_ID(), '_regular_price', true);}
-						$full_price = get_post_meta( get_the_ID(), '_regular_price', true);
-					?>
-					<div style="max-height:630px;">
-						<a href="<?php echo get_permalink( $loop->post->ID ) ?>"><img src="<?php echo get_the_post_thumbnail_url($loop->post->ID); ?>" alt="<?php the_title(); ?>"></a>
-					</div>
-					<div class="info">
-						<a href="<?php echo get_permalink( $loop->post->ID ) ?>"><div class="m-b-10"><?php the_title(); ?></div></a>
-						<? if ($full_price != $price) {?>
-						<div class="full_price"><span><? echo $full_price; ?> &#8381;</span></div>
-						<?}?>
-						<div class="price"><span><? echo $price; ?> &#8381;</span><div class="circle-green favorite"></div></div>
-					</div>
-					<?php endwhile; ?>
-					<?php wp_reset_query(); ?>
+				<?php endwhile; ?>
+				<?php wp_reset_query(); ?>
+			</div>
+			<div class="item">
+				<?php
+					$args = array(
+					'tax_query' => array(
+					'relation' => 'AND',
+					array(
+					'taxonomy' => 'product_cat',
+					'field' => 'slug',
+					'terms' => 'promo'
+					)),
+					'posts_per_page' => 1, 
+					'post_type' => 'product', 
+					'orderby' => 'title',
+					);
+					$loop = new WP_Query( $args );
+					while ( $loop->have_posts() ) : $loop->the_post();
+					global $product;
+					if (get_post_meta( get_the_ID(), '_sale_price', true)) {$price = get_post_meta( get_the_ID(), '_sale_price', true); } else
+						{$price = get_post_meta( get_the_ID(), '_regular_price', true);}
+					$full_price = get_post_meta( get_the_ID(), '_regular_price', true);
+				?>
+				<div style="max-height:630px;">
+					<a href="<?php echo get_permalink( $loop->post->ID ) ?>"><img src="<?php echo get_the_post_thumbnail_url($loop->post->ID); ?>" alt="<?php the_title(); ?>"></a>
 				</div>
-				
-				<!--<div class="items">
-					<div class="children__item">
-						<div class="children__image">
-							<div class="circle-green favorite"></div>
-							<a href="">
-								<img src="/wp-content/uploads/catalog-cat.jpg" alt="Комплексы Flowers Energy">
-							</a>
-						</div>
-						<a class="subcat__link m-b-10" href="">Товар 1</a>
-						<div class="price black"><span>700 &#8381;</span></div>
-					</div>
-					<div class="children__item">
-						<div class="children__image">
-							<div class="circle-green favorite"></div>
-							<a href="">
-								<img src="/wp-content/uploads/catalog-cat.jpg" alt="Комплексы Flowers Energy">
-							</a>
-						</div>
-						<a class="subcat__link m-b-10" href="">Товар 2</a>
-						<div class="price black"><span>480 &#8381;</span></div>
-					</div>
-					<div class="children__item">
-						<div class="children__image">
-							<div class="circle-green favorite"></div>
-							<a href="">
-								<img src="/wp-content/uploads/catalog-cat.jpg" alt="Комплексы Flowers Energy">
-							</a>
-						</div>
-						<a class="subcat__link m-b-10" href="">Товар 3</a>
-						<div class="price black"><span>1500 &#8381;</span></div>
-					</div>
-					<div class="children__item">
-						<div class="children__image">
-							<div class="circle-green favorite"></div>
-							<a href="">
-								<img src="/wp-content/uploads/catalog-cat.jpg" alt="Комплексы Flowers Energy">
-							</a>
-						</div>
-						<a class="subcat__link m-b-10" href="">Товар 4</a>
-						<div class="price black"><span>900 &#8381;</span></div>
-					</div>
-				</div>-->
-				<!--<div class="item">
-					<div>
-						<img src="/wp-content/uploads/offer-big-tmp.jpg" alt="">
-					</div>
-					<div class="info">
-						<div class="m-b-10">Товар 5</div>
-						<div class="price"><span>1100 &#8381;</span><div class="circle-green favorite"></div></div>
-					</div>
-				</div>-->
+				<div class="info">
+					<a href="<?php echo get_permalink( $loop->post->ID ) ?>"><div class="m-b-10"><?php the_title(); ?></div></a>
+					<? if ($full_price != $price) {?>
+					<div class="full_price"><span><? echo $full_price; ?> &#8381;</span></div>
+					<?}?>
+					<div class="price"><span><? echo $price; ?> &#8381;</span><div class="circle-green favorite"></div></div>
+				</div>
+				<?php endwhile; ?>
+				<?php wp_reset_query(); ?>
 			</div>
 		</div>
 	</div>
@@ -274,68 +240,66 @@ jQuery(document).ready(function($) {
 		<div class="three"></div>
 	</div>
     <div class="container">
-        <div class="row">
-            <div class="title">Наши<br/>преимущества:</div>
-			<div class="items">
+        <div class="title">Наши<br/>преимущества:</div>
+		<div class="items">
+			<div>
 				<div>
-					<div>
-						<img src="<?php echo get_stylesheet_directory_uri(); ?>/includes/images/advantage/01.png" alt="">
-					</div>
-					<div class="advantage-title">
-						Единственный специализированный компаундинг в России
-					</div>
-					<div class="advantage-n">
-						<i>1</i>
-					</div>
-					<div class="advantage-desc">
-						Природа уже создала все необходимое для здоровья и благополучия.
-						Мы производим лекарства нового поколения "для здоровья", а не "от болезней".
-					</div>
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/includes/images/advantage/01.png" alt="">
 				</div>
-				<div>
-					<div>
-						<img src="<?php echo get_stylesheet_directory_uri(); ?>/includes/images/advantage/02.png" alt="">
-					</div>
-					<div class="advantage-title">
-						 Фармацевтическая производственная лицензия
-					</div>
-					<div class="advantage-n">
-						<i>2</i>
-					</div>
-					<div class="advantage-desc">
-						Для Вас работает команда опытных врачей и фармацевтов. Для нас важно качество наших препаратов, мы аптека полного цикла - от заготовки сырья до производства персонального лекарства.
-					</div>
+				<div class="advantage-title">
+					Единственный специализированный компаундинг в России
 				</div>
-				<div>
-					<div>
-						<img src="<?php echo get_stylesheet_directory_uri(); ?>/includes/images/advantage/03.png" alt="">
-					</div>
-					<div class="advantage-title">
-						Персонализированные натуральные лекарства
-					</div>
-					<div class="advantage-n">
-						<i>3</i>
-					</div>
-					<div class="advantage-desc">
-						Мы придерживаемся принципа персонального изготовления препаратов под каждого пациента с его особыми потребностями. У каждого своя причина заболеть - и лекарство должно быть персональным.
-					</div>
+				<div class="advantage-n">
+					<i>1</i>
 				</div>
-				<div>
-					<div>
-						<img src="<?php echo get_stylesheet_directory_uri(); ?>/includes/images/advantage/04.png" alt="">
-					</div>
-					<div class="advantage-title">
-						Удобная доставка
-					</div>
-					<div class="advantage-n">
-						<i>4</i>
-					</div>
-					<div class="advantage-desc">
-						Аптека имеет официальное разрешение на диcтанционную продажу лекарственных средств. Мы осуществляем доставку по всем городам России, Казахстана, Белоруссии аккредитованной компанией Boxberry.
-					</div>
+				<div class="advantage-desc">
+					Природа уже создала все необходимое для здоровья и благополучия.
+					Мы производим лекарства нового поколения "для здоровья", а не "от болезней".
 				</div>
 			</div>
-        </div>
+			<div>
+				<div>
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/includes/images/advantage/02.png" alt="">
+				</div>
+				<div class="advantage-title">
+					 Фармацевтическая производственная лицензия
+				</div>
+				<div class="advantage-n">
+					<i>2</i>
+				</div>
+				<div class="advantage-desc">
+					Для Вас работает команда опытных врачей и фармацевтов. Для нас важно качество наших препаратов, мы аптека полного цикла - от заготовки сырья до производства персонального лекарства.
+				</div>
+			</div>
+			<div>
+				<div>
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/includes/images/advantage/03.png" alt="">
+				</div>
+				<div class="advantage-title">
+					Персонализированные натуральные лекарства
+				</div>
+				<div class="advantage-n">
+					<i>3</i>
+				</div>
+				<div class="advantage-desc">
+					Мы придерживаемся принципа персонального изготовления препаратов под каждого пациента с его особыми потребностями. У каждого своя причина заболеть - и лекарство должно быть персональным.
+				</div>
+			</div>
+			<div>
+				<div>
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/includes/images/advantage/04.png" alt="">
+				</div>
+				<div class="advantage-title">
+					Удобная доставка
+				</div>
+				<div class="advantage-n">
+					<i>4</i>
+				</div>
+				<div class="advantage-desc">
+					Аптека имеет официальное разрешение на диcтанционную продажу лекарственных средств. Мы осуществляем доставку по всем городам России, Казахстана, Белоруссии аккредитованной компанией Boxberry.
+				</div>
+			</div>
+		</div>
     </div>
 </div>
 
