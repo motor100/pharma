c501531f55275c90e11cc9131fc8010eb8d2c971
Evgeny <260443@mail.ru>
2024-08-16 15:20:49 +0500

    Contacts page

---
+++ wp-content/themes/store-child/page-kontakty.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Kontakty page
+ *
+ * @package WordPress
+ * @subpackage Store-Child Theme
+ * @since Store-Child Theme
+ */
+?>
+
+<?php get_header(); ?>
+  
+<div class="kontakty-page custom-page">
+  <div class="container">
+    <div class="storefront-breadcrumb">
+      <nav class="woocommerce-breadcrumb">
+        <a href="/">Главная</a>
+        <span class="breadcrumb-separator">></span>
+      </nav>
+    </div>
+  </div>
+  <div class="container">
+    <div class="page-title">Контакты</div>
+  </div>
+
+  <div class="contacts-section">
+    <div class="container">
+      <div class="contacts-section-subtitle">Телефоны</div>
+      <div class="phone-wrapper">
+        <a href="tel:+74959274928" class="contacts-section__phone">+7 (495) 927-4-928</a>
+        <span class="contacts-section__text">только по вопросам сотрудничества</span>
+      </div>
+      <div class="phone-wrapper">
+        <a href="tel:+79269880861" class="contacts-section__phone">+7 (926) 988-08-61</a>
+        <span class="contacts-section__text">по вопросам доставки и заказам</span>
+      </div>
+      <div class="contacts-section-subtitle">Адрес</div>
+      <div class="contacts-section__address">Москва, Алма-Атинская 9 к 2</div>
+      <div class="contacts-section__email">Почта: <span class="text-underline">info@naturapharma.ru</span></div>
+      <div class="contacts-section-subtitle">Режим работы</div>
+      <div class="working-time-item"><strong>Пн, Ср, Чт, Пт:</strong> С 10:00 до 20:00<br><strong>Вт:</strong> с 10:00 до 18:00</div>
+      <div class="working-time-item"><strong>Сб, Вс:</strong> выходной</div>
+    </div>
+  </div>
+  <div class="map-section">
+    <div class="container">
+      <div class="map">
+        <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A031a0b6256d1d2ec52e827b39594e15e4572c27bb05415c6a40851ea3d4d4027&amp;source=constructor" width="100%" height="400" frameborder="0"></iframe>
+      </div>
+    </div>
+  </div>
+
+  </div>
+</div>
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/store-child/page-sotrudnichestvo.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Sotrudnichestvo page
+ *
+ * @package WordPress
+ * @subpackage Store-Child Theme
+ * @since Store-Child Theme
+ */
+?>
+
+<?php get_header(); ?>
+  
+<div class="sotrudnichestvo-page custom-page">
+  <div class="container">
+    <div class="storefront-breadcrumb">
+      <nav class="woocommerce-breadcrumb">
+        <a href="/">Главная</a>
+        <span class="breadcrumb-separator">></span>
+      </nav>
+    </div>
+  </div>
+  <div class="container">
+    <div class="page-title">Сотрудничество</div>
+  </div>
+
+  <div class="description-section">
+    <div class="container">
+      <p class="text-bold">Мы первая производственная аптека в России, специализирующаяся на производстве новых классов натуропатических лекарств для здоровья и осознанной жизни, а не просто для лечения симптомов болезни.</p>
+      <p class="text-bold p-last">Мы создаем новую культуру здоровья и здорового образа жизни, осознанного отношения к жизни.</p>
+      <div class="sotrudnichestvo-subtitle">Мы приглашаем к сотрудничеству:</div>
+      <ul class="list">
+        <li class="list-item">Гомеопатические, натуропатические, фитотерапевтические аптеки.</li>
+        <li class="list-item">Клиники, медицинские центры, лечебно-профилактические учреждения, которым близки идеи натуропатии и психосоматики.</li>
+        <li class="list-item">Частнопрактикующих врачей, специалистов интегративной медицины, натуропатов, психологов, нутрициологов, массажистов, консультантов ЗОЖ и других специалистов помогающих профессий.</li>
+        <li class="list-item">Обучающие центры.</li>
+        <li class="list-item">Разработчиков новых натуропатических лекарственных средств и БАД, косметологических средств.</li>
+        <li class="list-item">Приверженцев принципов персонализированного подхода, понимающих важность индивидуального подхода и бережного отношения к здоровью.</li>
+      </ul>
+    </div>
+  </div>
+  <div class="frames-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <div class="frames-item">
+            <div class="frames-item__title">Мы создаем индивидуальные лекарства и комплексы</div>
+            <div class="frames-item__text">на основе природных компонентов высочайшего качества согласно российской и европейской фармакопеям.</div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="frames-item">
+            <div class="frames-item__title">Препараты изготавливаются вручную сертифицированными фармацевтами из сырья высокого качества</div>
+          </div>
+        </div>
+        <div class="col-md-8">
+          <div class="frames-item">
+            <div class="frames-item__title">Все наши рецептуры основаны на разработках российских и европейских практикующих врачей</div>
+            <div class="frames-item__text">не содержат консервантов, красителей и других небезопасных вспомогательных веществ.</div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="frames-item">
+            <div class="frames-item__title">Имеем большой опыт работы с биологическими препаратами</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="partnership-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <div class="partnership-text">По вопросам сотрудничества<br>Ознакомьтесь с документацией</div>
+        </div>
+        <div class="col-md-4">
+          <div class="partnership-btn">Заполнить анкету</div>
+          <div class="partnership-btn">Читать документ по сотрудничеству</div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  </div>
+</div>
+
+<?php get_footer(); ?>
