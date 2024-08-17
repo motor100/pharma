82c2f961769166d2181a62eb84606889cf6e3c1c
Evgeny <260443@mail.ru>
2024-08-17 23:45:44 +0500

    Dostavka i oplata page

---
+++ wp-content/themes/store-child/page-dostavka-i-oplata.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * Dostavka i oplata page
+ *
+ * @package WordPress
+ * @subpackage Store-Child Theme
+ * @since Store-Child Theme
+ */
+?>
+
+<?php get_header(); ?>
+  
+<div class="dostavka-i-oplata-page custom-page">
+  <div class="container">
+    <div class="storefront-breadcrumb">
+      <nav class="woocommerce-breadcrumb">
+        <a href="/">Главная</a>
+        <span class="breadcrumb-separator">></span>
+      </nav>
+    </div>
+  </div>
+  <div class="container">
+    <div class="page-title">Доставка и оплата</div>
+  </div>
+
+  <div class="dostavka-i-oplata-description-section">
+    <div class="container">
+      <div class="dostavka-i-oplata-description">Мы принимаем заказы на изготовление препаратов по персональным рецептам со всей территории России.</div>
+    </div>
+  </div>
+  
+  <div class="our-office-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-7">
+          <div class="dostavka-i-oplata-subtitle-small">Вы можете получить свой заказ в аптеке, адрес:</div>
+          <div class="dostavka-i-oplata-text">Москва, ул Алма-Атинская 9 к 2 ( вывеска Аптека, вход со стороны школы, м Алма-Атинская 10-12 мин пешком), будни с 10 до 20 часов.</div>
+          <a href="/kontakty" class="our-office-link" target="_blank">посмотреть на карте</a>
+        </div>
+        <div class="col-md-1"></div>
+        <div class="col-md-4">
+          <div class="dostavka-i-oplata-frame bold-frame">Доставка внутриаптечных заготовок и персональных препаратов осуществляется согласно закону №61-ФЗ «Об обращении лекарственных средств» от 12.04.10 с изменениями от 02.04.19.</div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="delivery-another-regions-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-7">
+          <div class="dostavka-i-oplata-subtitle-small">В зависимости от региона, Вы можете выбрать один из доступных способов получения препаратов:</div>
+          <ul class="dostavka-i-oplata-list">
+            <li class="list-item">самовывоз (Москва, Алма-Атинская 9 к 2)</li>
+            <li class="list-item">курьерская доставка специализированной компанией</li>
+            <li class="list-item">курьерская доставка с заданной температурой.</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="price-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-7">
+          <div class="dostavka-i-oplata-subtitle-small">Ориентировочная стоимость доставки:</div>
+          <ul class="dostavka-i-oplata-list">
+            <li class="list-item">Москва - 350 руб.</li>
+            <li class="list-item">Московская область 450 руб.</li>
+            <li class="list-item">Регионы РФ 600 руб.</li>
+            <li class="list-item">Отдаленные районы РФ 1100 руб (тариф рассчитывается автоматически СДЕК).</li>
+          </ul>
+        </div>
+        <div class="col-md-1"></div>
+        <div class="col-md-4">
+          <div class="dostavka-i-oplata-frame price-section-frame">Стоимость будет рассчитана автоматически во время оформления заказа.</div>
+          <div class="dostavka-i-oplata-frame price-section-frame">Заказы, стоимостью 7000 рублей доставляются бесплатно до пункта выдачи СДЕК удобного для Вас. Бесплатная доставка до двери для заказов от 10000 руб.</div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="delivery-time-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-7">
+          <div class="dostavka-i-oplata-subtitle-small">Сроки доставки зависят от региона, средние сроки по Москве- 1-2 рабочих дня, по Московской области 2-3 рабочих дня.</div>
+          <ul class="dostavka-i-oplata-list">
+            <li class="list-item">Срок изготовления препаратов зависит от количества заказов. В среднем 3-5 дней рабочих дней.</li>
+            <li class="list-item">При большом количестве заказов и сложных рецептурах срок изготовления составляет до 14 рабочих дней.</li>
+            <li class="list-item">Некоторые препараты требуют несколько дней для производства. Мы предупреждаем о таких позициях при оформлении заказа.</li>
+          </ul>
+        </div>
+        <div class="col-md-1"></div>
+        <div class="col-md-4">
+          <div class="dostavka-i-oplata-frame">Обратите внимание, курьерская компания отправляет номер для отслеживания на вайбер.</div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="payment-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-7">
+          <div class="dostavka-i-oplata-subtitle-small">Оплата</div>
+          <div class="dostavka-i-oplata-text">Оплачивайте и получайте заказ прямо в аптеке. Рассчитаться можно наличными, банковской картой или переводом по ссылке банка. Внутриаптечные заготовки всегда есть в наличии. Персональные препараты готовятся в течение 1-2 часов.</div>
+          <ul class="dostavka-i-oplata-list">
+            <li class="list-item">Оплата заказов с доставкой возможна через сайт или по ссылке банка на оплату. Ссылка на оплату отправляется при персональном заказе, при заказе по рецептам, при заказе с социальных сетей, почте на Вашу электронную почту.</li>
+            <li class="list-item">Обратите внимание, курьер не уполномочен принимать денежные средства, все заказы отправляются только после оплаты.</li>
+          </ul>
+        </div>
+        <div class="col-md-1"></div>
+        <div class="col-md-4">
+          <div class="dostavka-i-oplata-frame bold-frame">РАЗРЕШЕНИЕ<br>№ ДТ-77-000712 от 24.01.2023 г. на осуществление розничной торговли лекарственными препаратами для медицинского применения дистанционным способом</div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+</div>
+
+<?php get_footer(); ?>
