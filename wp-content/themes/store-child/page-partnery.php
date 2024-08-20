7a7b09df53aa266db8d284d2aba5cdf231d677ee
Evgeny <260443@mail.ru>
2024-08-20 22:27:41 +0500

    Partnery page

---
+++ wp-content/themes/store-child/page-partnery.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Partnery page
+ *
+ * @package WordPress
+ * @subpackage Store-Child Theme
+ * @since Store-Child Theme
+ */
+?>
+
+<?php get_header(); ?>
+  
+<div class="partnery-page custom-page">
+
+  <div class="container">
+    <div class="page-title">Партнеры</div>
+  </div>
+
+  <div class="partners-section">
+    <div class="container">
+      <div class="partner-item">
+        <div class="partner-item-title">Гомеопатическая аптека Медицинского гомеопатического Центра Наталии Радомской.</div>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="partner-item-address">
+              <div class="contacts-title">Адрес</div>
+              <div class="contacts-text">Москва, Боровское шоссе дом 56.</div>
+            </div>
+            <div class="partner-item-address">
+              <div class="contacts-title">Режим работы</div>
+              <div class="contacts-text"><strong>пн-пт:</strong> с 9:00 до 21:00</div>
+              <div class="contacts-text"><strong>сб-вс:</strong> с 10.00 до 20.00</div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="partner-item-address">
+              <div class="contacts-title">Телефон</div>
+              <div class="contacts-text">+7 (495) 732-41-61<br>наличие препаратов</div>
+              <div class="contacts-text">+7 (903) 011-24-49<br>(Ватсап) служба онлайн-заказов</div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="partner-item-address">
+              <div class="contacts-title">Сайт</div>
+              <a href="https://centr-radomskoy.ru/pharmacy" class="contacts-link" rel="nofollow" target="_blank">https://centr-radomskoy.ru/pharmacy</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="partner-item">
+        <div class="partner-item-title">ООО «Лекарственные средства ВАЛА-Р».</div>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="partner-item-address">
+              <div class="contacts-title">Адрес</div>
+              <div class="contacts-text">Москва, ул. Конёнкова, д. 11А</div>
+            </div>
+            <div class="partner-item-address">
+              <div class="contacts-title">Режим работы</div>
+              <div class="contacts-text"><strong>пн-пт:</strong>с 9:00 до 18:00</div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="partner-item-address">
+              <div class="contacts-title">Телефон</div>
+              <div class="contacts-text">+7 499 206-08-80</div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="partner-item-address">
+              <div class="contacts-title">Сайт</div>
+              <a href="https://rhythmus.ru" class="contacts-link" rel="nofollow" target="_blank">https://rhythmus.ru</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+<?php get_footer(); ?>
