a46e391d64c4b4ecbf70e1e643abdb6f51ef1996
Evgeny <260443@mail.ru>
2024-09-08 23:52:32 +0500

    Partnery page

--- wp-content/themes/store-child/page-partnery.php
+++ wp-content/themes/store-child/page-partnery.php
@@ -21,7 +21,7 @@
       <div class="partner-item">
         <div class="partner-item-title">Гомеопатическая аптека Медицинского гомеопатического Центра Наталии Радомской.</div>
         <div class="row">
-          <div class="col-md-4">
+          <div class="col-xl-4">
             <div class="partner-item-address">
               <div class="contacts-title">Адрес</div>
               <div class="contacts-text">Москва, Боровское шоссе дом 56.</div>
@@ -32,14 +32,14 @@
               <div class="contacts-text"><strong>сб-вс:</strong> с 10.00 до 20.00</div>
             </div>
           </div>
-          <div class="col-md-4">
+          <div class="col-xl-4">
             <div class="partner-item-address">
               <div class="contacts-title">Телефон</div>
               <div class="contacts-text">+7 (495) 732-41-61<br>наличие препаратов</div>
               <div class="contacts-text">+7 (903) 011-24-49<br>(Ватсап) служба онлайн-заказов</div>
             </div>
           </div>
-          <div class="col-md-4">
+          <div class="col-xl-4">
             <div class="partner-item-address">
               <div class="contacts-title">Сайт</div>
               <a href="https://centr-radomskoy.ru/pharmacy" class="contacts-link" rel="nofollow" target="_blank">https://centr-radomskoy.ru/pharmacy</a>
@@ -50,7 +50,7 @@
       <div class="partner-item">
         <div class="partner-item-title">ООО «Лекарственные средства ВАЛА-Р».</div>
         <div class="row">
-          <div class="col-md-4">
+          <div class="col-xl-4">
             <div class="partner-item-address">
               <div class="contacts-title">Адрес</div>
               <div class="contacts-text">Москва, ул. Конёнкова, д. 11А</div>
@@ -60,13 +60,13 @@
               <div class="contacts-text"><strong>пн-пт:</strong>с 9:00 до 18:00</div>
             </div>
           </div>
-          <div class="col-md-4">
+          <div class="col-xl-4">
             <div class="partner-item-address">
               <div class="contacts-title">Телефон</div>
               <div class="contacts-text">+7 499 206-08-80</div>
             </div>
           </div>
-          <div class="col-md-4">
+          <div class="col-xl-4">
             <div class="partner-item-address">
               <div class="contacts-title">Сайт</div>
               <a href="https://rhythmus.ru" class="contacts-link" rel="nofollow" target="_blank">https://rhythmus.ru</a>
