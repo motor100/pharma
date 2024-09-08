f07b58f55f4d341f3688f76abc1b2c358fc4ac2d
Evgeny <260443@mail.ru>
2024-09-08 23:41:19 +0500

    Dostavka i oplata page responsive

--- wp-content/themes/store-child/page-dostavka-i-oplata.php
+++ wp-content/themes/store-child/page-dostavka-i-oplata.php
@@ -25,13 +25,13 @@
   <div class="our-office-section dio-section">
     <div class="container">
       <div class="row">
-        <div class="col-md-7">
+        <div class="col-xl-7">
           <div class="dostavka-i-oplata-subtitle-small">Вы можете получить свой заказ в аптеке, адрес:</div>
           <div class="dostavka-i-oplata-text">Москва, ул Алма-Атинская 9 к 2 ( вывеска Аптека, вход со стороны школы, м Алма-Атинская 10-12 мин пешком), будни с 10 до 20 часов.</div>
           <a href="/kontakty/#map-section" class="our-office-link" target="_blank">посмотреть на карте</a>
         </div>
-        <div class="col-md-1"></div>
-        <div class="col-md-4">
+        <div class="col-xl-1"></div>
+        <div class="col-xl-4">
           <div class="dostavka-i-oplata-frame bold-frame">Доставка внутриаптечных заготовок и персональных препаратов осуществляется согласно закону №61-ФЗ «Об обращении лекарственных средств» от 12.04.10 с изменениями от 02.04.19.</div>
         </div>
       </div>
@@ -41,7 +41,7 @@
   <div class="delivery-another-regions-section dio-section">
     <div class="container">
       <div class="row">
-        <div class="col-md-7">
+        <div class="col-xl-7">
           <div class="dostavka-i-oplata-subtitle-small">В зависимости от региона, Вы можете выбрать один из доступных способов получения препаратов:</div>
           <ul class="dostavka-i-oplata-list">
             <li class="list-item">самовывоз (Москва, Алма-Атинская 9 к 2)</li>
@@ -56,7 +56,7 @@
   <div class="price-section dio-section">
     <div class="container">
       <div class="row">
-        <div class="col-md-7">
+        <div class="col-xl-7">
           <div class="dostavka-i-oplata-subtitle-small">Ориентировочная стоимость доставки:</div>
           <ul class="dostavka-i-oplata-list">
             <li class="list-item">Москва - 350 руб.</li>
@@ -65,8 +65,8 @@
             <li class="list-item">Отдаленные районы РФ 1100 руб (тариф рассчитывается автоматически СДЕК).</li>
           </ul>
         </div>
-        <div class="col-md-1"></div>
-        <div class="col-md-4">
+        <div class="col-xl-1"></div>
+        <div class="col-xl-4">
           <div class="dostavka-i-oplata-frame price-section-frame">Стоимость будет рассчитана автоматически во время оформления заказа.</div>
           <div class="dostavka-i-oplata-frame price-section-frame">Заказы, стоимостью 7000 рублей доставляются бесплатно до пункта выдачи СДЕК удобного для Вас. Бесплатная доставка до двери для заказов от 10000 руб.</div>
         </div>
@@ -77,7 +77,7 @@
   <div class="delivery-time-section dio-section">
     <div class="container">
       <div class="row">
-        <div class="col-md-7">
+        <div class="col-xl-7">
           <div class="dostavka-i-oplata-subtitle-small">Сроки доставки зависят от региона, средние сроки по Москве- 1-2 рабочих дня, по Московской области 2-3 рабочих дня.</div>
           <ul class="dostavka-i-oplata-list">
             <li class="list-item">Срок изготовления препаратов зависит от количества заказов. В среднем 3-5 дней рабочих дней.</li>
@@ -85,8 +85,8 @@
             <li class="list-item">Некоторые препараты требуют несколько дней для производства. Мы предупреждаем о таких позициях при оформлении заказа.</li>
           </ul>
         </div>
-        <div class="col-md-1"></div>
-        <div class="col-md-4">
+        <div class="col-xl-1"></div>
+        <div class="col-xl-4">
           <div class="dostavka-i-oplata-frame">Обратите внимание, курьерская компания отправляет номер для отслеживания на вайбер.</div>
         </div>
       </div>
@@ -96,7 +96,7 @@
   <div class="payment-section dio-section">
     <div class="container">
       <div class="row">
-        <div class="col-md-7">
+        <div class="col-xl-7">
           <div class="dostavka-i-oplata-subtitle-small">Оплата</div>
           <div class="dostavka-i-oplata-text">Оплачивайте и получайте заказ прямо в аптеке. Рассчитаться можно наличными, банковской картой или переводом по ссылке банка. Внутриаптечные заготовки всегда есть в наличии. Персональные препараты готовятся в течение 1-2 часов.</div>
           <ul class="dostavka-i-oplata-list">
@@ -104,8 +104,8 @@
             <li class="list-item">Обратите внимание, курьер не уполномочен принимать денежные средства, все заказы отправляются только после оплаты.</li>
           </ul>
         </div>
-        <div class="col-md-1"></div>
-        <div class="col-md-4">
+        <div class="col-xl-1"></div>
+        <div class="col-xl-4">
           <div class="dostavka-i-oplata-frame bold-frame">РАЗРЕШЕНИЕ<br>№ ДТ-77-000712 от 24.01.2023 г. на осуществление розничной торговли лекарственными препаратами для медицинского применения дистанционным способом</div>
         </div>
       </div>
@@ -115,7 +115,7 @@
   <div class="official-info">
     <div class="container">
       <div class="row">
-        <div class="col-md-7">
+        <div class="col-xl-7">
           <div class="dostavka-i-oplata-subtitle-small official-info-title">Официальная информация</div>
           <p class="company-info dostavka-i-oplata-text">
             Полное наименование — Общество с ограниченной ответственностью «ЗДОРОВЬЕ».<br>
